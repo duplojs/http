195bbc66d2379a754a74664c6a1a5a86217320de
mathcovax <campani.mathieu@gmail.com>
2025-11-04 18:27:29 +0000

    fix(1): build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,31 @@
 import typescript from '@rollup/plugin-typescript';
 import del from 'rollup-plugin-delete';
+import tscAlias from 'rollup-plugin-tsc-alias';
 import { defineConfig } from "rollup";
 
-export default defineConfig({
-	input: "scripts/index.ts",
-	output: [
-		{
-			dir: "dist",
-			format: "esm",
-			preserveModules: true,
-      		preserveModulesRoot: "scripts",
-			entryFileNames: "[name].mjs"
-		},
-		{
-			dir: "dist",
-			format: "cjs",
-			preserveModules: true,
-      		preserveModulesRoot: "scripts",
-			entryFileNames: "[name].cjs"
-		},
-	],
-	plugins: [
-		del({ targets: "dist" }),
-		typescript({ tsconfig: "tsconfig.build.json" }),
-	],
-});
+export default defineConfig([
+	{
+		input: "scripts/core/index.ts",
+		output: [
+			{
+				dir: "dist",
+				format: "esm",
+				preserveModules: true,
+				preserveModulesRoot: "scripts",
+				entryFileNames: "[name].mjs"
+			},
+			{
+				dir: "dist",
+				format: "cjs",
+				preserveModules: true,
+				preserveModulesRoot: "scripts",
+				entryFileNames: "[name].cjs"
+			},
+		],
+		plugins: [
+			del({ targets: "dist/core" }),
+			typescript({ tsconfig: "scripts/core/tsconfig.build.json" }),
+			tscAlias({ configFile: "scripts/core/tsconfig.build.json" }),
+		],
+	}
+]);
