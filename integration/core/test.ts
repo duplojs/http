8b7e1b5508b85936c0088296f51e348139f192f9
mathcovax <campani.mathieu@gmail.com>
2026-07-07 09:38:41 +0200

    feat(43): continue test

--- integration/core/test.ts
+++ integration/core/test.ts
@@ -1,7 +1,7 @@
 import { Typescript } from "@duplojs/data-parser-tools";
-import { findRouteTypeNodes, resolveTsconfig } from "@duplojs/http/codeGenerator";
-import { getCurrentWorkDirectoryOrThrow } from "@duplojs/server-utils";
-import { E } from "@duplojs/utils";
+import { resolveTsconfig } from "@duplojs/http/codeGenerator";
+import { getCurrentWorkDirectoryOrThrow, SF } from "@duplojs/server-utils";
+import { type AnyTuple, E, S } from "@duplojs/utils";
 
 const tsconfig = E.unwrapByInformationOrThrow(
 	resolveTsconfig(
@@ -11,23 +11,81 @@ const tsconfig = E.unwrapByInformationOrThrow(
 	"success",
 );
 
+const compilerOptions = {
+	...tsconfig.options,
+	noEmit: false,
+	declaration: true,
+	emitDeclarationOnly: true,
+	declarationDir: "core/temp",
+};
+
 const program = Typescript.createProgram({
 	rootNames: tsconfig.fileNames,
-	options: tsconfig.options,
+	options: compilerOptions,
 	projectReferences: tsconfig.projectReferences,
 });
 
-const checker = program.getTypeChecker();
+const projectFiles = program.getRootFileNames();
 
-const result = E.unwrapByInformationOrThrow(
-	await findRouteTypeNodes(
-		program,
-		checker,
-		{ includesFolders: ["core/routes"] },
-	),
-	"success",
-);
+const virtualFiles = new Map<string, {
+	content: string;
+	exportedRouteType: AnyTuple<string>;
+}>();
 
-for (const value of result) {
-	console.log(value);
+const routeDeclarationRegex = /(^|void\s+|(export\s+)?(const|var|let)\s+(?<varName>[a-zA-Z_][a-zA-Z0-9_]*)\s*=\s*)useRouteBuilder\s*\(\s*"/gm;
+
+for (const fileName of projectFiles) {
+	await E.rightAsyncPipe(
+		SF.readTextFile(fileName),
+		(content) => {
+			let exportedRouteType: AnyTuple<string> | undefined = undefined;
+
+			const result = S.replace(
+				content,
+				routeDeclarationRegex,
+				({ matchedValue, offset, namedGroups }) => {
+					const typeName = `Route_${virtualFiles.size}_${offset}`;
+					exportedRouteType = exportedRouteType
+						? [...exportedRouteType, typeName]
+						: [typeName];
+
+					const declarationRoute = namedGroups?.varName
+						? matchedValue
+						: `const ${typeName} = ${matchedValue}`;
+
+					return `export type ${typeName} = import("@duplojs/http/codeGenerator").RouteToClientRoute<typeof ${namedGroups?.varName || typeName}>;\n${declarationRoute}`;
+				},
+			);
+
+			if (exportedRouteType) {
+				virtualFiles.set(fileName, {
+					content: result,
+					exportedRouteType,
+				});
+			}
+		},
+	);
 }
+
+const host = Typescript.createCompilerHost(compilerOptions);
+const originalReadFile = host.readFile.bind(host);
+host.readFile = (fileName) => virtualFiles.get(fileName)?.content || originalReadFile(fileName);
+
+const newProgram = Typescript.createProgram({
+	rootNames: tsconfig.fileNames,
+	options: compilerOptions,
+	projectReferences: tsconfig.projectReferences,
+	host,
+	oldProgram: program,
+});
+
+// généré l'index
+// résoudre le path typescript
+// threeshaker les type pour garder que le néccésaire
+
+newProgram.emit(
+	undefined,
+	(fileName, content) => void console.log(fileName, content),
+	undefined,
+	true,
+);
