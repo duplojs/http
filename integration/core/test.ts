2fab7832aec7826e76a1288e7edccc96f003925c
mathcovax <campani.mathieu@gmail.com>
2026-07-14 22:07:26 +0200

    feat(43): use AST

--- integration/core/test.ts
+++ integration/core/test.ts
@@ -1,7 +1,7 @@
 import { Typescript } from "@duplojs/data-parser-tools";
 import { resolveTsconfig } from "@duplojs/http/codeGenerator";
 import { getCurrentWorkDirectoryOrThrow, SF } from "@duplojs/server-utils";
-import { type AnyTuple, E, S } from "@duplojs/utils";
+import { A, type AnyTuple, asyncPipe, E, G, innerPipe, isType, justExec, O, Path, pipe, promiseAll, S } from "@duplojs/utils";
 
 const tsconfig = E.unwrapByInformationOrThrow(
 	resolveTsconfig(
@@ -19,74 +19,180 @@ const compilerOptions = {
 	declarationDir: "core/temp",
 };
 
+await SF.remove(compilerOptions.declarationDir, { recursive: true });
+
 const program = Typescript.createProgram({
 	rootNames: tsconfig.fileNames,
 	options: compilerOptions,
 	projectReferences: tsconfig.projectReferences,
 });
 
-const projectFiles = program.getRootFileNames();
+const checker = program.getTypeChecker();
 
-const virtualFiles = new Map<string, {
-	content: string;
-	exportedRouteType: AnyTuple<string>;
-}>();
+let routeIndex = 0;
+const printer = Typescript.createPrinter();
+const virtualFiles = await asyncPipe(
+	tsconfig.fileNames,
+	A.chunk(10),
+	G.asyncMap(
+		innerPipe(
+			A.map((fileName) => {
+				const sourceFile = program.getSourceFile(fileName);
+				if (!sourceFile) {
+					return null;
+				}
 
-const routeDeclarationRegex = /(^|void\s+|(export\s+)?(const|var|let)\s+(?<varName>[a-zA-Z_][a-zA-Z0-9_]*)\s*=\s*)useRouteBuilder\s*\(\s*"/gm;
+				const newStatement = A.flatMap(
+					sourceFile.statements,
+					(statement) => {
+						if (
+							!Typescript.isExpressionStatement(statement)
+							|| !Typescript.isCallExpression(statement.expression)
+							|| !Typescript.isPropertyAccessExpression(statement.expression.expression)
+							|| statement.expression.expression.name.text !== "handler"
+						) {
+							return statement;
+						}
 
-for (const fileName of projectFiles) {
-	await E.rightAsyncPipe(
-		SF.readTextFile(fileName),
-		(content) => {
-			let exportedRouteType: AnyTuple<string> | undefined = undefined;
+						const signature = checker.getResolvedSignature(statement.expression);
+						if (!signature) {
+							return statement;
+						}
 
-			const result = S.replace(
-				content,
-				routeDeclarationRegex,
-				({ matchedValue, offset, namedGroups }) => {
-					const typeName = `Route_${virtualFiles.size}_${offset}`;
-					exportedRouteType = exportedRouteType
-						? [...exportedRouteType, typeName]
-						: [typeName];
+						const returnType = checker.getReturnTypeOfSignature(signature);
+						const typeSymbol = returnType.getSymbol();
+						if (!typeSymbol) {
+							return statement;
+						}
+						if (typeSymbol.getName() !== "Route") {
+							return statement;
+						}
 
-					const declarationRoute = namedGroups?.varName
-						? matchedValue
-						: `const ${typeName} = ${matchedValue}`;
+						const typeIdentifier = Typescript.factory.createIdentifier(
+							`Route_${routeIndex++}`,
+						);
 
-					return `export type ${typeName} = import("@duplojs/http/codeGenerator").RouteToClientRoute<typeof ${namedGroups?.varName || typeName}>;\n${declarationRoute}`;
-				},
-			);
+						return [
+							Typescript.factory.createVariableStatement(
+								undefined,
+								Typescript.factory.createVariableDeclarationList(
+									[
+										Typescript.factory.createVariableDeclaration(
+											typeIdentifier,
+											undefined,
+											undefined,
+											statement.expression,
+										),
+									],
+									Typescript.NodeFlags.Const,
+								),
+							),
+							Typescript.factory.createTypeAliasDeclaration(
+								[Typescript.factory.createToken(Typescript.SyntaxKind.ExportKeyword)],
+								typeIdentifier,
+								undefined,
+								Typescript.factory.createImportTypeNode(
+									Typescript.factory.createLiteralTypeNode(
+										Typescript.factory.createStringLiteral("@duplojs/http/codeGenerator"),
+									),
+									undefined,
+									Typescript.factory.createIdentifier("RouteToClientRoute"),
+									[
+										Typescript.factory.createTypeQueryNode(
+											typeIdentifier,
+											undefined,
+										),
+									],
+									false,
+								),
+							),
+						];
+					},
+				);
 
-			if (exportedRouteType) {
-				virtualFiles.set(fileName, {
-					content: result,
-					exportedRouteType,
-				});
-			}
-		},
-	);
-}
+				return O.entry(
+					fileName,
+					printer.printFile(
+						Typescript.factory.updateSourceFile(
+							sourceFile,
+							newStatement,
+						),
+					),
+				);
+			}),
+			promiseAll,
+		),
+	),
+	G.asyncFlat,
+	G.asyncFilter(isType("array")),
+	A.from,
+	(value) => new Map(value),
+);
 
 const host = Typescript.createCompilerHost(compilerOptions);
-const originalReadFile = host.readFile.bind(host);
-host.readFile = (fileName) => virtualFiles.get(fileName)?.content || originalReadFile(fileName);
+const originalGetSourceFile = host.getSourceFile.bind(host);
+host.getSourceFile = (
+	fileName,
+	languageVersion,
+	onError,
+	shouldCreateNewSourceFile,
+) => {
+	const content = virtualFiles.get(
+		fileName,
+	);
+
+	if (content !== undefined) {
+		return Typescript.createSourceFile(
+			fileName,
+			content,
+			languageVersion,
+			true,
+		);
+	}
+
+	return originalGetSourceFile(
+		fileName,
+		languageVersion,
+		onError,
+		shouldCreateNewSourceFile,
+	);
+};
 
 const newProgram = Typescript.createProgram({
 	rootNames: tsconfig.fileNames,
 	options: compilerOptions,
 	projectReferences: tsconfig.projectReferences,
 	host,
-	oldProgram: program,
 });
 
-// généré l'index
-// génére une deuxéime fois mais en résolvant le type pars inférence pour le rendre plus simple
-// résoudre le path typescript
-// threeshaker les type pour garder que le néccésaire
+// // généré l'index
+// // génére une deuxéime fois mais en résolvant le type pars inférence pour le rendre plus simple
+// // résoudre le path typescript
+// // threeshaker les type pour garder que le néccésaire
+
+const result: any[] = [];
 
 newProgram.emit(
 	undefined,
-	(fileName, content) => void console.log(fileName, content),
+	(fileName, content) => {
+		console.log(fileName);
+
+		result.push(
+			justExec(async() => {
+				const folder = Path.getParentFolderPath(fileName);
+
+				if (folder === null) {
+					return SF.writeTextFile(fileName, content);
+				}
+
+				await SF.makeDirectory(folder, { recursive: true });
+
+				return SF.writeTextFile(fileName, content);
+			}),
+		);
+	},
 	undefined,
 	true,
 );
+
+await promiseAll(result);
